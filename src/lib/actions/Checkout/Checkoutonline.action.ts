2048ba6fa31c2afc2d13f72ec1bdf9b76e64671d
markonci <markonci06@gmail.com>
2025-09-24 15:35:56 +0300

    try resolve

--- src/lib/actions/Checkout/Checkoutonline.action.ts
+++ src/lib/actions/Checkout/Checkoutonline.action.ts
@@ -7,7 +7,7 @@ import { CheckOutschemaType } from "@/schema/CheckOut.schema"
 export default async function Checkoutonline(cartId:string,formvalues:CheckOutschemaType) {
 const token = await getMyToken()
 if(!token)throw new Error('Must Login')
-     const res=await fetch(`${process.env.API}/${cartId}?url=${process.env.NEXT_URL}`,{
+     const res=await fetch(`${process.env.API}/orders/checkout-session/${cartId}?url=${process.env.NEXT_URL}`,{
     method:'POST',
     headers:{token,
             "Content-Type": "application/json",

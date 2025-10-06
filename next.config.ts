b7c17f7540fae811d86850abaebaa3868793a717
markonci <markonci06@gmail.com>
2025-10-06 17:26:57 +0300

    try

--- next.config.ts
+++ next.config.ts
@@ -14,3 +14,12 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [new URL('https://ecommerce.routemisr.com/**/**')],
+//   },};
+
+// export default nextConfig;

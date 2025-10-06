ee0f5ba0ff48dcd817c3f3f3d8e04597ebaaa23e
markonci <markonci06@gmail.com>
2025-10-06 16:49:56 +0300

    try

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "ecommerce.routemisr.com",
-        pathname: "/Route-Academy-products/**", // ممكن تسيبها "/**" لو عايز تسمح بكل الصور
-      },
-    ],
-  },
+images: {
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "ecommerce.routemisr.com",
+      pathname: "/**",
+    },
+  ],
+},
 };
 
 export default nextConfig;
